ce38b2e4c72618e185a814d76d88019775762cd5
dimas <dimas.rasyach@gmail.com>
2026-05-21 19:35:36 +0700

    fix: fix and drop logo android before splashscreen animation

--- Aplikasi Kalkulator Turunan/app/build.gradle.kts
+++ Aplikasi Kalkulator Turunan/app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
